aba66398fb1c637f9fd1481d40455ceddd059267
jmonroybarrios <132100376+jmonroybarrios@users.noreply.github.com>
2024-03-19 10:44:42 -0500

    segundo commit
    segunda carga de archivos

---
+++ led1.cpp
@@ -0,0 +1,11 @@
+if (x > 120) digitalWrite(LEDpin, HIGH);
+
+if (x > 120)
+digitalWrite(LEDpin, HIGH);
+
+if (x > 120){ digitalWrite(LEDpin, HIGH); }
+
+if (x > 120){
+  digitalWrite(LEDpin1, HIGH);
+  digitalWrite(LEDpin2, HIGH);
+}                                 // all are cor
